45a919216616d9a67ba6a7e9f50595366ffe1528
samuel <samawyck@gmail.com>
2025-05-02 13:14:20 -0500

    Create fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,87 @@
+import { useState } from 'react';
 import './App.css';
+import Header from './components/header';
 
 function App() {
-  return (
-    <>
-    
-    </>
-  )
+  	const [url, setUrl] = useState(null);
+  	const picAPIKey = "7c3ntBcQtpYfaylcsqUfE04D4vEjtkowgP5AcSVOjECiKNeT1EQldZBV";
+
+
+	async function getData(url, options) {
+		const response = await fetch(url, options);
+		const responseJSON = await response.json();
+		return responseJSON;
+	};
+
+
+	async function artClickHandler() {
+		const querys = [
+			"renaissance", "renaissance%20paintings",
+			"renaissance%20artwork", "cathedral%20exterior"
+		];
+		const query = getRandomQuery(querys);
+
+		const url = `https://api.pexels.com/v1/search?query=${query}&per_page=12`;
+		const options = {mode: "cors", headers: {"Authorization": picAPIKey}};
+		const data = await getData(url, options);
+		const photoData = parseData(data);
+		console.log(photoData);
+	};
+
+	
+	async function picClickHandler() {
+		const querys = [
+			"wild&20animals", "nature%20wallpaper", "cities",
+			"landscape", "fish", "africa%20wildlife",
+			"china%20landscape", "food", "abstract",
+			"medieval&20castle", "horse", "bird"
+		];
+		const query = getRandomQuery(querys);
+
+		const url = `https://api.pexels.com/v1/search?query=${query}&per_page=12`;
+		const options = {mode: "cors", headers: {"Authorization": picAPIKey}};
+		const data = await getData(url, options);
+		const photoData = parseData(data);
+		console.log(photoData);
+	};
+
+
+	function parseData(data) {
+		const parsedData = [];
+		for (let photoInfo of data.photos) {
+			const photoObject = {};
+
+			photoObject["alt"] = photoInfo.alt;
+			photoObject["author"] = photoInfo.photographer;
+			photoObject["imgSrc"] = photoInfo.src.landscape;
+			photoObject["id"] = photoInfo.id;
+			parsedData.push(photoObject);
+		}
+		return parsedData;
+	};
+
+
+	function getRandomQuery(querys) {
+		const randIdx = randInt(0, querys.length - 1);
+		return querys[randIdx];
+	};
+
+
+	function randInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+
+
+
+
+	return (
+		<>
+		<Header artClickHandler={artClickHandler} picClickHandler={picClickHandler}></Header>
+		</>
+	);
 }
 
 export default App
