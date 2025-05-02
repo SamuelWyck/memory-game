292272b1e9be34d9c4e585e2a216237947aa6678
samuel <samawyck@gmail.com>
2025-05-02 14:03:31 -0500

    Create default data

---
+++ src/defaultPics.js
@@ -0,0 +1,77 @@
+const defaultData = [
+    {
+        "alt": "Famous Renaissance painting by Raphael depicting various philosophers in a classical setting.",
+        "author": "Pixabay",
+        "id": 159862,
+        "imgSrc": "https://images.pexels.com/photos/159862/art-school-of-athens-raphael-italian-painter-fresco-159862.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+    },
+    {
+        "alt": "Black and white photo of historic stone sculptures showcasing intricate craftsmanship.",
+        "author": "Pixabay",
+        "id": 36006,
+        "imgSrc": "https://images.pexels.com/photos/36006/renaissance-schallaburg-figures-facade.jpg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "Beautiful Venice Grand Canal view with Basilica di Santa Maria della Salute in daylight, perfect for travel and architecture theme.",
+        "author": "Anastasiya Lobanovskaya",
+        "id": 804954,
+        "imgSrc": "https://images.pexels.com/photos/804954/pexels-photo-804954.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "A dramatic close-up of a renowned statue in Paris expressing frustration.",
+        "author": "karatara",
+        "id": 931317,
+        "imgSrc": "https://images.pexels.com/photos/931317/pexels-photo-931317.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "A detailed view of a marble sculpture in Rome showcasing Renaissance artistry and Roman heritage.",
+        "author": "Griffin Wooldridge",
+        "id": 2676582,
+        "imgSrc": "https://images.pexels.com/photos/2676582/pexels-photo-2676582.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "A stunning mural of Jesus Christ within a church dome, showcasing Renaissance religious art.",
+        "author": "Emre Can Acer",
+        "id": 2079661,
+        "imgSrc": "https://images.pexels.com/photos/2079661/pexels-photo-2079661.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "Iconic view of Florence Cathedral's dome against a clear Tuscan sky, a masterpiece of Renaissance architecture.",
+        "author": "Maegan White",
+        "id": 981682,
+        "imgSrc": "https://images.pexels.com/photos/981682/pexels-photo-981682.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "Detailed view of a baroque interior showcasing ornate columns and a classical statue.",
+        "author": "Ekaterina Astakhova",
+        "id": 3450330,
+        "imgSrc": "https://images.pexels.com/photos/3450330/pexels-photo-3450330.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "Luxurious interior featuring ornate gold design and classic furniture in a regal setting.",
+        "author": "Alexandros Chatzidimos",
+        "id": 3652784,
+        "imgSrc": "https://images.pexels.com/photos/3652784/pexels-photo-3652784.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "A marble bust sculpture displayed in a dimly lit, classic art gallery.",
+        "author": "Michel Caicedo",
+        "id": 3713476,
+        "imgSrc": "https://images.pexels.com/photos/3713476/pexels-photo-3713476.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "Pensive woman sitting with classical sculptures, reflecting beauty and art.",
+        "author": "cottonbro studio",
+        "id": 3778550,
+        "imgSrc": "https://images.pexels.com/photos/3778550/pexels-photo-3778550.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    },
+    {
+        "alt": "Explore the stunning Neuschwanstein Castle, a fairy-tale landmark in Bavaria, Germany. Perfect for travel and history enthusiasts.",
+        "author": "Lum3n",
+        "id": 234307,
+        "imgSrc": "https://images.pexels.com/photos/234307/pexels-photo-234307.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+    }
+];
+
+
+export default defaultData;
